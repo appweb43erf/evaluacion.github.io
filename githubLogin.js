743e1dc29fe08cb3451f40a3e279771c0522a924
appweb43erf <151330536+appweb43erf@users.noreply.github.com>
2023-11-29 13:39:48 -0600

    Add files via upload

---
+++ githubLogin.js
@@ -0,0 +1,21 @@
+import { GitHubAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { auth } from './firebase.js';
+import { showMessage } from './showMessage.js'
+
+const githubButton = document.querySelector('#guthubLogin')
+
+githubButton.addEventListener('click', async () => {
+    const provider = new GitHubAuthProvider()
+
+    try {
+        const credentials = await signInWithPopup(auth, provider)
+        console.log(credentials)
+
+        const modal = bootstrap.Modal.getInstance(document.querySelector('#signinModal'))
+        modal.hide()
+
+        showMessage('Welcome' + credentials.user.displayName, 'success')
+    }catch (error){
+        console.log(error)
+    }
+})
