6cde7833de6f6fb992a5068441c4fb3a1027a70a
appweb43erf <151330536+appweb43erf@users.noreply.github.com>
2023-11-29 13:47:01 -0600

    Update githubLogin.js

--- githubLogin.js
+++ githubLogin.js
@@ -1,21 +1,38 @@
-import { GitHubAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { GithubAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { auth } from './firebase.js';
-import { showMessage } from './showMessage.js'
 
-const githubButton = document.querySelector('#githubLogin')
+const facebookButton = document.querySelector('#githubLogin');
 
-githubButton.addEventListener('click', async () => {
-    const provider = new GitHubAuthProvider()
+facebookButton.addEventListener('click', async () => {
+    const provider = new GithubAuthProvider();
 
     try {
-        const credentials = await signInWithPopup(auth, provider)
-        console.log(credentials)
+        const result = await signInWithPopup(auth, provider);
+        const credentials = GithubAuthProvider.credentialFromResult(result);
+        const accessToken = credentials.accessToken;
 
-        const modal = bootstrap.Modal.getInstance(document.querySelector('#signinModal'))
-        modal.hide()
+        // Obtiene la información del usuario
+        const user = result.user;
 
-        showMessage('Welcome' + credentials.user.displayName, 'success')
-    }catch (error){
-        console.log(error)
+        // Puedes acceder a más información del usuario usando
+        // result.additionalUserInfo.profile
+
+        console.log(user);
+        console.log(accessToken);
+
+        // Cierra el modal después de iniciar sesión
+        const modal = new bootstrap.Modal(document.querySelector('#signinModal'));
+        modal.hide();
+    } catch (error) {
+        if (error.code === 'auth/cancelled-popup-request') {
+            // El usuario canceló la solicitud del popup
+            console.log('Solicitud de popup cancelada por el usuario');
+        } else if (error.code === 'auth/popup-closed-by-user') {
+            // El usuario cerró el popup
+            console.log('Popup cerrado por el usuario');
+        } else {
+            // Otro error, imprímelo en la consola
+            console.error(error);
+        }
     }
-})
+});
