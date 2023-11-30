ab4fb553b9e26c73d30fcc497a46d3fc3bc83421
rickmrform <formasjmm@gmail.com>
2023-11-29 21:59:57 -0600

    iuiuiasa

--- seguridad.js
+++ seguridad.js
@@ -1,50 +1,83 @@
-import { getAuth, getFirestore } from "../lib/fabrica.js";
-
-
-const firestore = getFirestore();
-const daoUsuario = firestore.collection("Usuario");
-
-export async function iniciaSesión() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: "select_account" });
-  await getAuth().signInWithRedirect(provider);
-}
-
-export async function tieneRol(usuario, roles) {
-  if (usuario && usuario.email) {
-    const rolIds = await cargaRoles(usuario.email);
-
-    // Verifica si el conjunto de roles incluye "admin"
-    if (rolIds.has("admin")) {
-      // El usuario tiene el rol de administrador, redirige a "crud.html"
-      window.location.href = "crud.html";
-      return true;
+import { getAuth,
+    getFirestore
+  } from "../lib/fabrica.js";
+  import {
+    muestraError
+  } from "../lib/util.js";
+  
+  const firestore = getFirestore();
+  const daoUsuario = firestore.
+    collection("Usuario");
+  
+  export async function iniciaSesión() {
+    /** Tipo de autenticación de
+     * usuarios. En este caso es con
+     * Google.
+     * @type {import("../lib/tiposFire.js").GoogleAuthProvider} */
+    const provider =
+      // @ts-ignore
+      new firebase.auth.
+        GoogleAuthProvider();
+    /* Configura el proveedor de
+     * Google para que permita
+     * seleccionar de una lista. */
+    provider.setCustomParameters(
+      { prompt: "select_account" });
+    await getAuth().
+      signInWithRedirect(provider);
+  }
+  /** @param {import(
+      "../lib/tiposFire.js").User}
+      usuario
+   * @param {string[]} roles
+   * @returns {Promise<boolean>} */
+  export async function
+    tieneRol(usuario, roles) {
+    if (usuario && usuario.email) {
+      const rolIds =
+        await cargaRoles(
+          usuario.email);
+      for (const rol of roles) {
+        if (rolIds.has(rol)) {
+          return true;
+        }
+      }
+      alert("No autorizado.");
+      location.href = "index.html";
     } else {
-      // El usuario no tiene el rol de administrador, redirige a "usuario.html"
-      window.location.href = "usuario.html";
-      return false;
+      iniciaSesión();
     }
-  } else {
-    // Si no hay usuario o correo, inicia sesión
-    iniciaSesión();
     return false;
   }
-}
-
-export async function terminaSesión() {
-  try {
-    await getAuth().signOut();
-  } catch (e) {
-    muestraError(e);
+  
+  export async function
+    terminaSesión() {
+    try {
+      await getAuth().signOut();
+    } catch (e) {
+      muestraError(e);
+    }
   }
-}
-
-export async function cargaRoles(email) {
-  let doc = await daoUsuario.doc(email).get();
-  if (doc.exists) {
-    const data = doc.data();
-    return new Set(data.rolIds || []);
-  } else {
-    return new Set();
+  
+  /** @param {string} email
+   * @returns {Promise<Set<string>>}
+   */
+  export async function
+    cargaRoles(email) {
+    let doc =
+      await daoUsuario.
+        doc(email).
+        get();
+    if (doc.exists) {
+      /**
+       * @type {
+          import("./tipos.js").
+          Usuario} */
+      const data = doc.data();
+      return new Set(
+        data.rolIds || []);
+    } else {
+      return new Set();
+    }
   }
-}
+  
