6a23033f9ca3dd82ae459f6b7d2a59397d73a656
rickmrform <formasjmm@gmail.com>
2023-11-29 23:10:26 -0600

    pol

--- googleLogin.js
+++ googleLogin.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(user);
             console.log(accessToken);
   
-            window.location.href = "Usuario.html";
+         
             // Cierra el modal después de iniciar sesión
         } catch (error) {
             // Maneja los errores específicos de la autenticación de Google
