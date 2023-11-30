5ef53424d346909917b9713aac148d5b0f49d657
rickmrform <formasjmm@gmail.com>
2023-11-29 21:23:48 -0600

    lolo

--- backToLogin.js
+++ backToLogin.js
@@ -1,6 +1,9 @@
-// backToLogin.js
+
+import { } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"
+
 document.addEventListener('DOMContentLoaded', function () {
-    const backToLoginButton = document.querySelector('#logout');
+    const backToLoginButton = document.querySelector('#backToLogin');
     if (backToLoginButton) {
       backToLoginButton.addEventListener('click', () => {
         // Redirige al usuario a la página de inicio de sesión
